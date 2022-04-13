658f57158e81978281071ff24c5664180a7ba008
carmelofileccia <carmelofileccia@gmail.com>
2022-04-13 16:11:25 +0200

    Add site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ function Footer () {
     return (
         <div className="footer">
            <h2 className='footerP'>carmelofileccia@gmail.com</h2>
+           <h4 className='footerSub'>www.elevengraphics.it</h4>
 
         </div>
     )
